3d4022b51eeff3f9b3ff78d0beb38037005535c9
Will Czifro <will@czifrotech.com>
2017-06-22 16:02:28 -0700

    Added buffer init, logging, end packet handling and state changing

--- fsharp2/MCDTP.Net.PacketManagement/server.fs
+++ fsharp2/MCDTP.Net.PacketManagement/server.fs
@@ -3,11 +3,27 @@ namespace MCDTP.Net.PacketManagement
   open MCDTP.Logging
   open MCDTP.Utility
   open MCDTP.Net.Protocol
+  open System.Threading
 
   [<AutoOpen>]
   module Server =
 
     type PacketManager with
+
+      member this.InitializeBuffer() =
+        let initializer =
+          async {
+            let or' = this.InternalOnReplenish
+            let fs = this.OnReplenish
+            let size = this.ActionThreshold*2
+            do! PacketManagerImpl.asyncReplenishBuffer size fs or'
+            this.BufferStateLock
+            |> Sync.write(fun () ->
+              this.BufferState <- BufferState.Idle
+            )
+          }
+        initializer
+        |> Async.RunSynchronously
 
       member this.TryPullPacket() =
         let pOp =
@@ -16,40 +32,55 @@ namespace MCDTP.Net.PacketManagement
             match this.Buffer with
             | x::xs ->
               this.Buffer <- xs
+              let msg = sprintf "Popped packet from buffer: %A" x
+              this.NetworkLogger.LogPlainMessage(LogLevel.None,msg)
               Some x
             | _ -> None
           )
         this.BufferStateLock
         |> Sync.write(fun () ->
-          if List.length this.Buffer > this.ActionThreshold
-            && this.BufferState = BufferState.Idle && this.Mode = Server then
+          let doTryReplenish =
+            (pOp.IsNone && this.BufferState = BufferState.Idle)
+              || (List.length this.Buffer > this.ActionThreshold
+                  && this.BufferState = BufferState.Idle && this.Mode = Server)
+          if doTryReplenish then
+            this.BufferState <- BufferState.Replenishing
             this.TryReplenish()
         )
         pOp
 
       member internal this.TryReplenish() =
-        let startSeqNum =
-          (this.BufferLock
-          |> Sync.read(fun () -> this.Buffer)
-          |> List.rev
-          |> List.head).seqNum
-        this.InternalReplenish(startSeqNum)
+        this.InternalReplenish()
 
       member internal this.InternalOnReplenish(packets:UdpPacket list) =
-        this.BufferLock
-        |> Sync.write(fun () ->
-          this.Buffer <- this.Buffer@packets
-        )
         if packets |> List.isEmpty then
-          this.Mode <- ServerRetransmitting
+          if this.Timer |> isNull then
+            let cb = TimerCallback(this.RetransmitCallback)
+            // Using only a due time, periodic is self managed
+            this.Timer <- new Timer(cb,null,this.RetransmitInterval,Timeout.Infinite)
+          let packetOp = this.EndPacket
+          if packetOp.IsSome then
+            let message = "Buffered end packet"
+            this.NetworkLogger.LogPlainMessage(LogLevel.Info,message)
+            this.RetransmitLock
+            |> Sync.write(fun () ->
+              this.Retransmit <- [packetOp.Value]@this.Retransmit
+            )
+        else
+          this.BufferLock
+          |> Sync.write(fun () ->
+            this.Buffer <- this.Buffer@packets
+          )
+          let message = sprintf "Loaded %d packets" (List.length packets)
+          this.NetworkLogger.LogPlainMessage(LogLevel.Info,message)
 
-      member internal this.InternalReplenish(startSeqNum:int64) =
+      member internal this.InternalReplenish () =
         let replenisher =
           async {
             let or' = this.InternalOnReplenish
             let fs = this.OnReplenish
             let size = this.ActionThreshold
-            do! PacketManagerImpl.asyncReplenishBuffer startSeqNum size fs or'
+            do! PacketManagerImpl.asyncReplenishBuffer size fs or'
             this.BufferStateLock
             |> Sync.write(fun () ->
               this.BufferState <- BufferState.Idle
@@ -59,6 +90,3 @@ namespace MCDTP.Net.PacketManagement
         |> Async.StartChild
         |> Async.RunSynchronously
         |> ignore
-
-      member this.HasSwitchedToRetransmitMode() =
-        this.Mode = ServerRetransmitting
